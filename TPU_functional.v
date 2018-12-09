2b7048969d0fac0b8b4384a5dbdc337085d67450
Ethan Williams <ewilli23@nd.edu>
2018-12-09 17:43:46 -0500

    TPU Functional v1 done

--- TPU_functional.v
+++ TPU_functional.v
@@ -11,11 +11,10 @@ module TPU_functional (
 	output reg [15:0] out;
 
 	reg [31:0] accumulate;
-	// reg [7:0] mult_input_1;
-	// reg [7:0] mult_input_2;
 	reg [7:0] mult_output;
 	reg [5:0] mantissa;
 	reg [5:0] exponent;
+	reg [31:0] bitshift_out;
 
 	always @(*)
 	begin
@@ -38,6 +37,7 @@ module TPU_functional (
 	end
 	always @(posedge sync)
 	begin
+		ready <= 0;
 		mult_output[7] = input1[7] ^ input2[7]; //sign bit
 		exponent <= input1[6:3] + input2[6:3];
 		mantissa <= input1[2:0] + input2[2:0];
@@ -57,11 +57,25 @@ module TPU_functional (
 		// check for exponent overflow
 		if (exponent[5:4] != 2'b00) begin
 			error = 1;
+			mult_output <= 8'b11111111; //NaN (infinity maybe?)
 		end
 		else begin
 			mult_output[6:3] <= exponent[3:0];
 			mult_output[2:0] <= mantissa[2:0];
 		end
+
+		// Bit shift to create integer
+		bitshift_out = mult_output[2:0] << mult_output[6:3];
+		if (mult_output[7] == 1) begin // negative
+			bitshift_out ^= 32'b11111111111111111111111111111111;
+			bitshift_out += 1;
+		end
+
+		// accumulate
+		accumulate += bitshift_out;
+
+		ready <= 1;
+
 	end
 
 
