3bfc676304d83c3d02471a4355d4feadfc5f04dd
Jack Mazanec <jmazane1@nd.edu>
2018-12-11 17:25:46 -0500

    updated

--- structural/tpu_structural.v
+++ structural/tpu_structural.v
@@ -7,33 +7,35 @@ module TPU_structural (
 	input wire [7:0] input1;
 	input wire [7:0] input2;
 
-	output reg error;
+	output wire error;
 	output wire [15:0] out;
 
 	wire [31:0] accumulate;
 
+	wire [31:0] accumulate_wire;
+
 	wire [31:0] product_regular;
 	wire [31:0] product_tcomp;
-	wire [31:0] accumulate_output;
 	wire [7:0] 	mult_output;
 	wire [5:0] 	mantissa;
 	wire [4:0] 	exponent;
 	wire sign;
 
 	xor			x1(sign, input1[7], input2[7]);
-	add4b		a4(exponent, input1[6:3], input2[6:3]);
+	add4b		a1(exponent, input1[6:3], input2[6:3]);
 	mult3b		m1(mantissa, input1[2:0], input2[2:0]);
 
-	shifter		s1(mantissa, exponent, error, product_regular);
+	shifter		s1(product_regular, mantissa, exponent, error);
 
-	xor32b		x2(product_regular, sign, product_tcomp);
+	xor32b		x2(product_tcomp, product_regular, sign);
 
-	tad32		t1(product_tcomp, accumulate, reset, sync, accumulate_output);
+	add32b		a2(accumulate_wire, accumulate, product_tcomp);
 
-	dec			d1(accumulate_output, out_HL, out);
+	mux2_32b 	m2(accumulate_wire, accumulate_wire, 0, reset);
 
-	mux2		m1(sync, ready);
+	d_flip_flop	d1(accumulate, clk, accumulate_wire);
 
 
+	mux2_16b	m3(out, accumulate, out_HL);
 
 endmodule
