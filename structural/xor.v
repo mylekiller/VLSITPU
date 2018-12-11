96e6d8e7a6b7c2749fd77bba3347acf6d3fc0308
Jack Mazanec <jmazane1@nd.edu>
2018-12-11 16:20:30 -0500

    32 bit complete

--- structural/xor.v
+++ structural/xor.v
@@ -1,22 +1,45 @@
 module xor32b (
-	number, sign, output
+	number, sign, number_2comp
 	);
 
 	input wire [31:0] number;
 	input wire sign;
 	output wire [31:0] number_2comp;
 
-	wire [31:0] sign_shift;
-	wire [31:0] clause1;
-	wire [31:0] clause2;
+	xor 	x1(number[0], sign, number_2comp[0]);
+	xor 	x2(number[1], sign, number_2comp[1]);
+	xor 	x3(number[2], sign, number_2comp[2]);
+	xor 	x4(number[3], sign, number_2comp[3]);
+	xor 	x5(number[4], sign, number_2comp[4]);
+	xor 	x6(number[5], sign, number_2comp[5]);
+	xor 	x7(number[6], sign, number_2comp[6]);
+	xor 	x8(number[7], sign, number_2comp[7]);
 
-	shift_left_32	sl1(sign, sign_shift);
+	xor 	x9(number[8], sign, number_2comp[8]);
+	xor 	x10(number[9], sign, number_2comp[9]);
+	xor 	x11(number[10], sign, number_2comp[10]);
+	xor 	x12(number[11], sign, number_2comp[11]);
+	xor 	x13(number[12], sign, number_2comp[12]);
+	xor 	x14(number[13], sign, number_2comp[13]);
+	xor 	x15(number[14], sign, number_2comp[14]);
+	xor 	x16(number[15], sign, number_2comp[15]);
 
-	inv32b		i1(number, not_number);
-	inv32b		i2(sign_shift, not_sign_shift);
+	xor 	x17(number[16], sign, number_2comp[16]);
+	xor 	x18(number[17], sign, number_2comp[17]);
+	xor 	x19(number[18], sign, number_2comp[18]);
+	xor 	x20(number[19], sign, number_2comp[19]);
+	xor 	x21(number[20], sign, number_2comp[20]);
+	xor 	x22(number[21], sign, number_2comp[21]);
+	xor 	x23(number[22], sign, number_2comp[22]);
+	xor 	x24(number[23], sign, number_2comp[23]);
 
-	and32b		a1(sign_shift, not_number, clause1);
-	and32b		a2not_sign_shift, number, clause2);
-	or32b		o1(clause1, clause2, number_2comp);
+	xor 	x25(number[24], sign, number_2comp[24]);
+	xor 	x26(number[25], sign, number_2comp[25]);
+	xor 	x27(number[26], sign, number_2comp[26]);
+	xor 	x28(number[27], sign, number_2comp[27]);
+	xor 	x29(number[28], sign, number_2comp[28]);
+	xor 	x30(number[29], sign, number_2comp[29]);
+	xor 	x31(number[30], sign, number_2comp[30]);
+	xor 	x32(number[31], sign, number_2comp[31]);
 
 endmodule
