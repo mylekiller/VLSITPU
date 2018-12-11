1cffc3df6250c9d8aeb714c037969904ddf3cacf
Jack Mazanec <jmazane1@nd.edu>
2018-12-11 16:11:36 -0500

    remove 1 bit xor

--- structural/xor.v
+++ structural/xor.v
@@ -1,25 +1,4 @@
-module xor1b (
-	s1, s2, output
-	);
-
-	input wire s1;
-	input wire s2;
-	output wire sign;
-
-	wire not_s1, not_s2, clause1, clause2;
-
-
-	inv 		i1(s1, not_s1);
-	inv 		i2(s2, not_s2);
-
-	and			a1(s1, not_s2, clause1);
-	and			a2(not_s1, s2, clause2);
-	or  		o1(c1, c2, sign);
-
-endmodule
-
-
-module xor30b (
+module xor32b (
 	number, sign, output
 	);
 
