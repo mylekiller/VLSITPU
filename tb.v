c2a12a42d6e1308838a9a5b71788b4c4660db50c
Kyle <kmille42@nd.edu>
2018-12-10 19:57:46 -0500

    Added one more test case added some comments

--- tb.v
+++ tb.v
@@ -12,7 +12,12 @@ module tpu_tb;
 	initial
 	begin
 		clk = 0; reset = 1; out_HL = 0;
-		#10 reset = 0; input1 = 8'b00001101; input2 = 8'b00001111; sync = 1;
+		#10 reset = 0; input1 = 8'b00001101; input2 = 8'b00001111; sync = 1; //output should be 11000000 (192)
+		#10 sync = 0;
+		#10 out_HL = 1;
+		#10 out_HL = 0;
+		#10 input1 = 8'b00101001; input2 = 8'b00101111; sync = 1; //output should be 1111000000000 (7680)
+		// when added to the previous number should be 1111011000000 (7872)
 		#10 sync = 0;
 		#10 out_HL = 1;
 		#10 out_HL = 0;
