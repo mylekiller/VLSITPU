e4287a7f07728e5b0c8d1eca65eda99715b54530
Kyle <kmille42@nd.edu>
2018-12-11 17:23:11 -0500

    Fixed it

--- TPU_functional.v
+++ TPU_functional.v
@@ -1,18 +1,18 @@
 module TPU_functional (
-	reset, clk, sync, out_HL,
-	ready, error,
+	reset, clk, out_HL,
+	error,
 	input1, input2, out
 	);
 
-	input wire reset, clk, sync, out_HL;
-	output reg ready, error;
+	input wire reset, clk, out_HL;
+	output reg error;
 	input wire [7:0] input1;
 	input wire [7:0] input2;
 	output reg [15:0] out;
 
 	reg [31:0] accumulate;
-	reg [7:0] mult_output;
-	reg [5:0] mantissa;
+	reg signbit;
+	reg [7:0] mantissa;
 	reg [5:0] exponent;
 	reg [31:0] bitshift_out;
 
@@ -26,54 +26,29 @@ module TPU_functional (
 		end
 	end
 	
-	always @(sync, reset)
+	always @(posedge clk)
 	begin
 		if (reset == 1) begin
 			accumulate = 0;
 			error = 0;
-			ready = 1;
-			mult_output = 0;
+			signbit = 0;
 		end
-		else if (sync == 1)
+		else
 			begin
-				ready = 0;
-				mult_output[7] = input1[7] ^ input2[7]; //sign bit
+				signbit = input1[7] ^ input2[7]; //sign bit
 				exponent = input1[6:3] + input2[6:3];
-				mantissa = input1[2:0] * input2[2:0];
-				// bit shift mantissa and update exponent
-				if (mantissa[5] == 1) begin
-					mantissa = mantissa >> 3;
-					exponent = exponent + 3;
-				end
-				else if (mantissa[4] == 1) begin
-					mantissa = mantissa >> 2;
-					exponent = exponent + 2;
-				end
-				else if (mantissa[3] == 1) begin
-					mantissa = mantissa >> 1;
-					exponent = exponent + 1;
-				end
-				// check for exponent overflow
-				if (exponent[5:4] != 2'b00) begin
-					error = 1;
-					mult_output = 8'b11111111; //NaN (infinity maybe?)
-				end
-				else begin
-					mult_output[6:3] = exponent[3:0];
-					mult_output[2:0] = mantissa[2:0];
-				end
+				mantissa = {1'b1,input1[2:0]} * {1'b1, input2[2:0]};
 
 				// Bit shift to create integer
-				bitshift_out = {1'b1, mult_output[2:0]} << (mult_output[6:3] - 1);
-				if (mult_output[7] == 1) begin // negative
-					bitshift_out = bitshift_out ^ 32'b11111111111111111111111111111111;
+				bitshift_out = mantissa << (exponent - 2);
+				if (signbit == 1) begin // negative
+					bitshift_out = bitshift_out ^ 32'd4294967295;
 					bitshift_out = bitshift_out + 1;
 				end
 				
 				// accumulate
 				accumulate = accumulate + bitshift_out;
 
-				ready = 1;
 
 			end
 		end
--- tb.v
+++ tb.v
@@ -1,26 +1,27 @@
 `timescale 1 ns / 1 ns
 module tpu_tb;
 
-	reg reset, clk, sync, out_HL;
-	wire ready, error;
+	reg reset, clk, out_HL;
+	wire error;
 	reg [7:0] input1;
 	reg [7:0] input2;
 	wire [15:0] out;
 	
-	TPU_functional uut(reset, clk, sync, out_HL, ready, error, input1, input2, out);
+	TPU_functional uut(reset, clk, out_HL, error, input1, input2, out);
 	
 	initial
 	begin
-		clk = 0; reset = 1; out_HL = 0;
-		#10 reset = 0; input1 = 8'b00001101; input2 = 8'b00001111; sync = 1; //output should be 11000000 (192)
-		#10 sync = 0;
-		#10 out_HL = 1;
-		#10 out_HL = 0;
-		#10 input1 = 8'b00101001; input2 = 8'b00101111; sync = 1; //output should be 1111000000000 (7680)
+		clk = 1; reset = 1; out_HL = 0; input1 = 8'b00001101; input2 = 8'b00001111;
+		#5 reset = 0; //output should be 11000000 (195)
+		#5 out_HL = 1;
+		#5 out_HL = 0;
+		#5 input1 = 8'b00101001; input2 = 8'b00101111; //output should be 1111000000000 (7680)
 		// when added to the previous number should be 1111011000000 (7872)
-		#10 sync = 0;
-		#10 out_HL = 1;
-		#10 out_HL = 0;
+		#5 out_HL = 1; input1 = 8'd0; input2 = 8'd0;
+		#5 out_HL = 0;
+		#5 input1 = 8'b10001001; input2 = 8'b00001001;
+		#5 out_HL = 1;
+		#5 out_HL = 0; input1 = 8'd0; input2 = 8'd0;
 	end
 	
 	always begin
