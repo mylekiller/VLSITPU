a8bf17643cb235ad781da9afbed62f684e400604
Kyle <kmille42@nd.edu>
2018-12-11 20:05:10 -0500

    Added More Test Cases

--- tb.v
+++ tb.v
@@ -12,18 +12,21 @@ module tpu_tb;
 	initial
 	begin
 		clk = 1; reset = 1; out_HL = 0; input1 = 8'b00001101; input2 = 8'b00001111;
-		#5 reset = 0; //output should be 11000000 (195)
+		#5 reset = 0; //output should be (195)
 		#5 out_HL = 1;
 		#5 out_HL = 0;
-		#5 input1 = 8'b00101001; input2 = 8'b00101111; //output should be 1111000000000 (7680)
-		// when added to the previous number should be 1111011000000 (7872)
+		#5 input1 = 8'b00101001; input2 = 8'b00101111; //output should be (34560)
+		// when added to the previous number should be (34755)
 		#5 out_HL = 1; input1 = 8'd0; input2 = 8'd0;
 		#5 out_HL = 0;
-		#5 input1 = 8'b10001001; input2 = 8'b00001001;
+		#5 input1 = 8'b10001001; input2 = 8'b00001001; //-9*9=-81 total output should be 34755-81 = 34674
 		#5 out_HL = 1;
 		#5 out_HL = 0; input1 = 8'd0; input2 = 8'd0; reset = 1;
-		#10 reset = 0; input1 = 8'b00000001; input2 = 8'b00000001;
-		#10 input1 = 8'd0; input2 = 8'd0;
+		#10 reset = 0; input1 = 8'b00000001; input2 = 8'b00000001; //output should be (1)
+		#10 input1 = 8'b10001001; input2 = 8'b10001001; //-9*-9=81 Plus the previous 1 stored should be 82
+		#5 out_HL = 1;
+		#5 input1 = 8'd0; input2 = 8'd0; out_HL = 0;
+		#10 $stop;
 		
 	end
 	
